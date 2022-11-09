e12c79c1202cfa34d245a1a943de5d4faa23ff18
Robert0321 <shat0806449@gmail.com>
2022-11-09 15:29:51 +0800

    sliderV1.0

--- slide/ViewController.swift
+++ slide/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var moveSlider: UISlider!
+    @IBOutlet weak var gril: UIImageView!
+    @IBOutlet weak var child: UIImageView!
+    @IBOutlet weak var cookies: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func moveSlider(_ sender: UISlider) {
+        
+        let speed = sender.value * 0.3
+        child.frame.origin.x = CGFloat(152+speed)
+        gril.frame.origin.x = CGFloat(496-speed)
+        cookies.alpha = CGFloat(sender.value/360)
+    }
 }
 
